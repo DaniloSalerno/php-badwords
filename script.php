8ddb45a3d8ae1a3f2106296cbd9d598aeb183389
DaniloSalerno <salerno.danilo@hotmail.it>
2023-10-09 14:53:02 +0200

    add string with censured word in script.php

--- script.php
+++ script.php
@@ -1,3 +1,16 @@
+<?php
+
+$paragraf = $_GET['paragraf'];
+$bad_word = $_GET['bad_word'];
+
+$paragraf_array = explode(' ', $paragraf);
+$censured = str_replace($bad_word, '***', $paragraf_array);
+var_dump($censured)
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,7 +27,12 @@
     <div>
         Contiene <?php echo strlen($_GET['paragraf']); ?> caratteri
     </div>
-
+    <div>
+        La frase che hai scritto censurata: <?php echo implode(' ', $censured); ?>
+    </div>
+    <div>
+        Contiene <?php echo strlen(implode(' ', $censured)); ?> caratteri
+    </div>
 </body>
 
 </html>
